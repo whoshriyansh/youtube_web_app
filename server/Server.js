d487b8f766c228b9f4c010da77e7f1685e8d68df
Shriyansh Lohia <132228919+whoshriyansh@users.noreply.github.com>
2024-07-06 06:55:56 +0530

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoute);
 app.use("/api/videos", videoRoutes);
